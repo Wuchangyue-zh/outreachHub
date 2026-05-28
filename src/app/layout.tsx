424a2ca1eeb8efff093b2265a3bfbd65cf784806
wuchangyue <wuchangyue@jafron.com>
2026-05-28 15:11:07 +0800

    Add favicon, manifest.json, and PWA support
    - Add SVG favicon with OutreachHub branding
    - Add manifest.json for PWA support with app shortcuts
    - Update layout.tsx with correct favicon paths
    - Add icon generation HTML tool for creating PNG icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,9 +78,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
         {/* Favicon */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
 
         {/* Manifest */}
         <link rel="manifest" href="/manifest.json" />
